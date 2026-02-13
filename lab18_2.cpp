812f1b6fbc18725eb063892600c5b5498e1ea11b
9CPNine <nattaphon_ch@cmu.ac.th>
2026-02-13 22:53:51 +0700

    lab18_2_completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
 
+    double r1_right = R1->x + R1->w;
+    double r2_right = R2->x + R2->w;
+    double w_overlap = (r1_right < r2_right ? r1_right : r2_right) - (R1->x > R2->x ? R1->x : R2->x);
 
+    double r1_bottom = R1->y - R1->h;
+    double r2_bottom = R2->y - R2->h;
+    double h_overlap = (R1->y < R2->y ? R1->y : R2->y) - (r1_bottom > r2_bottom ? r1_bottom : r2_bottom);
 
+    if(w_overlap <= 0 || h_overlap <= 0) return 0;
+
+    return w_overlap * h_overlap;
 }
